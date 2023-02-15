d1615e9f6bcc705837febef201eb636183088325
ElenaReyes19 <84975845+ElenaReyes19@users.noreply.github.com>
2023-02-14 20:48:51 -0600

    Add files via upload

---
+++ ExamenOrdinarioMike-main/ExamenOrdinario/src/main/java/mx/edu/utez/ExamenOrdinario/dto/TransactionDto.java
@@ -0,0 +1,29 @@
+package mx.edu.utez.ExamenOrdinario.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mx.edu.utez.ExamenOrdinario.model.transaction.Transaction;
+import mx.edu.utez.ExamenOrdinario.model.user.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TransactionDto {
+    private Long id;
+    private Long id_usuario;
+    private Long id_product;
+    private Date fecha;
+    @NotNull
+    @NotBlank
+    private List<Transaction> transactions;
+
+
+}
